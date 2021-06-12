6dc1608c582b466b4ae31d1be9f66e0254d02d2f
Jones Napoleon Autumn <13518086@std.stei.itb.ac.id>
2021-06-12 22:48:40 +0700

    header basic nav

--- src/components/layout/Navbar/Header.js
+++ src/components/layout/Navbar/Header.js
@@ -6,6 +6,19 @@ import { ReactComponent as HelpSvg } from "../../../assets/images/Help Icon.svg"
 import logoUrl from "../../../assets/images/Company Logo@2x.png";
 
 const Header = ({ auth, t, signOut }) => {
+  // console.log(temp);
+  const dropdownSelector = document.querySelector(
+    ".dropdown-menu.nav-dropdown"
+  );
+
+  const toggleDropdown = (bool) => {
+    // const sel = dropdownSelector.style.display;
+    if (bool) dropdownSelector.style.display = "block";
+    else dropdownSelector.style.display = "none";
+    // dropdownSelector.style.display =
+    //   dropdownSelector.style.display === "block" ? "none" : "block";
+    // console.log(dropdownSelector);
+  };
   return (
     <div className="navbar-wrapper">
       <div className="nav-area">
@@ -31,7 +44,43 @@ const Header = ({ auth, t, signOut }) => {
           <div className="item-center super-pt-1 last-nav-child">
             <img className="rounded-img" src={auth?.picture} alt="" />
             <div>{auth?.fullname}</div>
-            <div>&gt;</div>
+            <div className="dropright">
+              <span role="button" class="navlink expanded" id="comp-nav">
+                &gt;
+              </span>
+              <ul id="navdropdown">
+                <li class="navlink inactive">
+                  <Link className="dropdownede-item" to={FRONTEND_URL.settings}>
+                    {t("header.myProfile")}
+                  </Link>
+                </li>
+                <li class="navlink inactive">
+                  <div className="droefpdown-item" onClick={signOut}>
+                    {t("header.signOut")}
+                  </div>
+                </li>
+              </ul>
+            </div>
+            {/* <div className="dropright">
+              <div
+                className="dropdown-toggle"
+                type="button"
+                onMouseLeave={() => toggleDropdown(!true)}
+                onMouseOver={() => toggleDropdown(true)}
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              ></div>
+              <div className="dropdown-menu nav-dropdown">
+                <Link className="dropdown-item" to={FRONTEND_URL.settings}>
+                  {t("header.myProfile")}
+                </Link>
+                <div className="dropdown-item" onClick={signOut}>
+                  {t("header.signOut")}
+                </div>
+              </div>
+            </div> */}
+            {/* <div onClick={}>&gt;</div> */}
             <div>
               <HelpSvg />
             </div>
