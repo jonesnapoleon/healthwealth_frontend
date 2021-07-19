e4ae78683992e5abcdf47f6c0510996c0a88141c
jonathangun <jonathanyudigun@gmail.com>
2021-07-19 12:36:04 +0700

    WIP signaturepad

--- src/components/@Settings/@Signature/index.js
+++ src/components/@Settings/@Signature/index.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { useModal } from "../../../contexts/ModalContext";
 import ImageUpload from "../../commons/ImageUpload";
+import SignatureModal from "../../commons/SignatureModal";
 import "./signature.css";
 
 const Signature = () => {
   const { t } = useTranslation();
+  const { setInnerComponent, show } = useModal();
 
   return (
     <div className="signature-container">
       <div>
         <div className="lead">{t("settings.signature.text")}</div>
-        <ImageUpload meta={{ head: t("settings.signature.addSignature") }} />
+        <ImageUpload
+          meta={{
+            head: t("settings.signature.addSignature")
+          }}
+          onClick={() => {
+            setInnerComponent(<SignatureModal />);
+            show?.set(true);
+          }} />
       </div>
       <div>
         <div className="lead">{t("settings.signature.initial")}</div>
