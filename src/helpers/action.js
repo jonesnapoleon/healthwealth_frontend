cc8e4978c200154906e1b836358b4cfd73b0da4b
Jones Napoleon Autumn <13518086@std.stei.itb.ac.id>
2021-11-03 15:17:54 +0700

    first commit

--- src/helpers/action.js
+++
@@ -1,9 +0,0 @@
-export const openWA = (wa, name, place1, place2, finalUrl) => {
-  window.open(
-    `https://api.whatsapp.com/send?phone=${encodeURIComponent(
-      wa
-    )}&text=${encodeURIComponent(place1)}%20${encodeURIComponent(
-      name
-    )}%20${encodeURIComponent(place2)}%20${finalUrl}`
-  );
-};
